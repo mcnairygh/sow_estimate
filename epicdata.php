27f742caa0e63d2b7eb709690fb8864df3463628
mcnairygh <67652040+mcnairygh@users.noreply.github.com>
2020-07-02 09:17:42 -0500

    Create epicdata.php

---
+++ epicdata.php
@@ -0,0 +1,19 @@
+<?php
+
+//Epic Data ID,EPIC NAME, EPIC AC, EPIC CAT, Min Stories, Max Stories
+
+myObj = {
+  "epic":"Create VPC",
+  "reuse":Y,
+  "category":30,
+  "ac": {
+    "ac1":"Feature 1",
+    "ac2":"Feature 2",
+    "ac3":"Feature 3"
+  }
+ }
+
+
+
+
+?>
