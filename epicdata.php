139e388fdc5f4ba71717bebf8d6c27507f5b69d3
mcnairygh <67652040+mcnairygh@users.noreply.github.com>
2020-07-02 09:31:45 -0500

    Update epicdata.php

--- epicdata.php
+++ epicdata.php
@@ -3,15 +3,30 @@
 //Epic Data ID,EPIC NAME, EPIC AC, EPIC CAT, Min Stories, Max Stories
 
 myObj = {
-  "epic":"Create VPC",
-  "reuse":Y,
-  "category":30,
-  "ac": {
-    "ac1":"Feature 1",
-    "ac2":"Feature 2",
-    "ac3":"Feature 3"
-  }
- }
+  "epic":{
+   "Create VPC" [
+     { "reuse":Y,
+       "category":BCF,
+       "ac": {
+           "ac1":"Feature 1",
+           "ac2":"Feature 2",
+           "ac3":"Feature 3"
+       }
+       "minstories":5
+       "maxstories":7
+     }],
+   "Create Pipeline" [
+     { "reuse":Y,
+       "category":TFM,
+       "ac": {
+           "ac1":"Feature 1",
+           "ac2":"Feature 2",
+           "ac3":"Feature 3"
+           "ac4":"Feature 4"
+       }
+       "minstories":7
+       "maxstories":12
+     }]
 
 
 
